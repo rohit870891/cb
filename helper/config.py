6e764e32714eb5bcca149a1e0a35ec5e212e5beb
rohit870891 <koyeb77user@gmail.com>
2025-05-08 01:18:47 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -2,7 +2,7 @@
 from . import *
 
 try:
-    APP_ID = config("APP_ID", default=21624420, cast=int)
+    APP_ID = config("APP_ID", default=20718334, cast=int)
     API_HASH = config("API_HASH", default="4e81464b29d79c58d0ad8a0c55ece4a5")
     BOT_TOKEN = config("BOT_TOKEN", default="8065030679:AAE-Vvvejh6maceHLBamYQPOYIyC-sMUM2w")
     OWNER = config("OWNER_ID", default=7328629001, cast=int)
