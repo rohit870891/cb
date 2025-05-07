fe7712239f720d743ed96f2bc4702ffc97d3bbce
rohit870891 <koyeb77user@gmail.com>
2025-05-08 01:12:56 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -1,25 +1,12 @@
-#    This file is part of the Compressor distribution.
-#    Copyright (c) 2021 Danish_00
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-#    License can be found in < https://github.com/1Danish-00/CompressorBot/blob/main/License> .
 
 from . import *
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
-    OWNER = config("OWNER_ID", default=1322549723, cast=int)
-    LOG = config("LOG_CHANNEL", cast=int)
+    APP_ID = config("APP_ID", default=21624420, cast=int)
+    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    BOT_TOKEN = config("BOT_TOKEN", default="8065030679:AAE-Vvvejh6maceHLBamYQPOYIyC-sMUM2w")
+    OWNER = config("OWNER_ID", default=7328629001, cast=int)
+    LOG = config("LOG_CHANNEL", default=-1002200709110, cast=int)
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
